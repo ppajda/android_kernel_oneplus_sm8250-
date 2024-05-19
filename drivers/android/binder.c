a503a692195196eb12cafc1b29c8386d31147d4e
Pajdek <pajdek@gmail.com>
2024-05-19 20:16:03 +0200

    Disable OPLUS_HANS
    Signed-off-by: Pajdek <pajdek@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3821,7 +3821,7 @@ static void binder_transaction(struct binder_proc *proc,
 			&& (task_uid(target_proc->tsk).val > MIN_USERAPP_UID)
 			&& (proc->pid != target_proc->pid)
 			&& is_frozen_tg(target_proc->tsk)) {
-			hans_report(SYNC_BINDER, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, "SYNC_BINDER", -1);
+//			hans_report(SYNC_BINDER, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, "SYNC_BINDER", -1);
 		}
 #endif /*OPLUS_FEATURE_HANS_FREEZE*/
 
@@ -3830,7 +3830,7 @@ static void binder_transaction(struct binder_proc *proc,
 			&& target_proc
 			&& (task_uid(target_proc->tsk).val > MIN_USERAPP_UID || task_uid(target_proc->tsk).val == HANS_SYSTEM_UID) //uid >10000
 			&& is_belong_cpugrp(target_proc->tsk)) {
-			hans_report(SYNC_BINDER_CPUCTL, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, "SYNC_BINDER_CPUCTL", -1);
+//			hans_report(SYNC_BINDER_CPUCTL, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, "SYNC_BINDER_CPUCTL", -1);
 		}
 #endif
 
@@ -4088,7 +4088,7 @@ static void binder_transaction(struct binder_proc *proc,
 				}
 				if (i == INTERFACETOKEN_BUFF_SIZE) buf[i-1] = '\0';
 			}
-			hans_report(ASYNC_BINDER, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, buf, tr->code);
+//			hans_report(ASYNC_BINDER, task_tgid_nr(proc->tsk), task_uid(proc->tsk).val, task_tgid_nr(target_proc->tsk), task_uid(target_proc->tsk).val, buf, tr->code);
 		}
 	}
 #endif /*OPLUS_FEATURE_HANS_FREEZE*/
@@ -7053,7 +7053,7 @@ static void hans_check_uid_proc_status(struct binder_proc *proc, enum message_ty
 			uid = task_uid(thread->task).val;
 			if (!empty) {
 				binder_inner_proc_unlock(proc);
-				hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_THREAD", 1);
+//				hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_THREAD", 1);
 				return;
 			}
 
@@ -7065,7 +7065,7 @@ static void hans_check_uid_proc_status(struct binder_proc *proc, enum message_ty
 					/* only report incoming binder call */
 					spin_unlock(&btrans->lock);
 					binder_inner_proc_unlock(proc);
-					hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_STACK", 1);
+//					hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_STACK", 1);
 					return;
 				}
 				spin_unlock(&btrans->lock);
@@ -7078,7 +7078,7 @@ static void hans_check_uid_proc_status(struct binder_proc *proc, enum message_ty
 	if (proc->tsk != NULL && !empty) {
 		uid = task_uid(proc->tsk).val;
 		binder_inner_proc_unlock(proc);
-		hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_PROC", 1);
+//		hans_report(type, -1, -1, -1, uid, "FROZEN_TRANS_PROC", 1);
 		return;
 	}
 	binder_inner_proc_unlock(proc);
